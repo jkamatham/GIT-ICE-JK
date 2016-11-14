743ccc86ebd950fcedb4245f53f8b070e0127b55
Student <s525891@ch140031m.nwmissouri.edu>
2016-11-14 16:45:12 -0600

    Added SeaVC and renamed first an second view controllers

--- Git-ICE-JK/BeeVC.swift
+++ Git-ICE-JK/BeeVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  Git-ICE-JK
 //
 //  Created by Kamatham,Jeevitha on 11/14/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Git-ICE-JK/EhVC.swift
+++ Git-ICE-JK/EhVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  Git-ICE-JK
 //
 //  Created by Kamatham,Jeevitha on 11/14/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class EhVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
